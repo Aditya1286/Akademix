ef3644feb9f9d0098a86616a26fda0b5616329aa
pumpumx <trishuchibhot14@gmail.com>
2025-03-24 00:36:35 +0530

    Login page modified || Spline initiated || Working on attendance management

--- Akademix/src/main.jsx
+++ Akademix/src/main.jsx
@@ -6,17 +6,14 @@ import {store} from './Components/Store/store.js'
 import {
   Home,
   Login,
-  Navbar,
-  Sidebar,
-  // TodoCalendar
 } from './Components/index.js'
 import { Provider } from 'react-redux'
 import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter, Router } from 'react-router-dom'
 const route = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Home' element={<Home />} />
+      <Route path='/' element={<Login/>}/>
+      <Route path='/home' element={<Home/>} />
     </>
   )
 )
